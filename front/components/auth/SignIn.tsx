d8145e5cd9f02e4c8758ef3e9de98d31f654288e
ClementLmd <clement.laumond@gmail.com>
2024-11-03 17:35:11 +0100

    create basic sign in and sign up components

---
+++ front/components/auth/SignIn.tsx
@@ -0,0 +1,36 @@
+'use client';
+import { useState } from 'react';
+import styles from './signIn.module.css';
+import { Button } from '../ui/button';
+import { Input } from '../ui/input';
+
+export function SignIn() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignIn = () => {};
+
+  return (
+    <div className={styles.auth}>
+      <div className={styles.inputContainer}>
+        <Input
+          type="text"
+          className={styles.input}
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+          placeholder="Username"
+        />
+      </div>
+      <div className={styles.inputContainer}>
+        <Input
+          type="password"
+          className={styles.input}
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          placeholder="Password"
+        />
+      </div>
+      <Button onClick={handleSignIn}>Sign in</Button>
+    </div>
+  );
+}
