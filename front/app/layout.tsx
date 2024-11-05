aa7d043949e60c774b4963b20055448d7fc424be
ClementLmd <clement.laumond@gmail.com>
2024-11-05 09:58:14 +0100

    wip css modifications

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -4,6 +4,19 @@ import HeaderLayout from '../components/header/HeaderLayout';
 import FooterLayout from '../components/footer/FooterLayout';
 import { Provider } from 'react-redux';
 import { store } from '../redux/store';
+import { Inter, Poppins } from 'next/font/google';
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['400', '600'],
+  variable: '--font-inter',
+});
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['700'],
+  variable: '--font-poppins',
+});
 
 export default function RootLayout({
   children,
@@ -13,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Provider store={store}>
-        <body>
+        <body className={`${inter.variable} ${poppins.variable}`}>
           <HeaderLayout />
           <div className="container">{children}</div>
           <FooterLayout />
