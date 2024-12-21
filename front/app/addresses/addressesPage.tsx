8b36927946e654d3218974c01866acd0b3c3b726
clement lmd <clement.laumond@gmail.com>
2024-12-21 13:59:04 +0100

    addresses are displayed in a table

--- front/app/addresses/addressesPage.tsx
+++ front/app/addresses/addressesPage.tsx
@@ -1,14 +1,38 @@
+'use client';
 // Route: /addresses
 
+import { Address } from '@shared/types/address';
 import { AddAddress } from '../../components/form/addAddress';
+import CustomTable from '../../components/ui/custom-table';
 import styles from './addresses.module.css';
+import { selectAddresses } from '../../features/addresses/addresses.selectors';
+import { useSelector } from 'react-redux';
 
 export default function AddressesPage() {
+  const addresses: Address[] = useSelector(selectAddresses);
+  console.log(addresses);
+
+  const columns = [
+    { key: 'blockchain', label: 'Blockchain', sortable: true },
+    { key: 'address', label: 'Address', sortable: true },
+    { key: 'addressName', label: 'Address name', sortable: true },
+  ];
+
+  const data = addresses.map((address: Address, key: number) => ({
+    id: key,
+    blockchain: address.blockchain,
+    address: address.address,
+    addressName: address.addressName,
+  }));
+
   return (
     <div className={styles.main}>
-      <AddAddress blockchain="Bitcoin" />
-      <AddAddress blockchain="Ethereum" />
-      <AddAddress blockchain="Solana" />
+      <div className={styles.addAddress}>
+        <AddAddress blockchain="Bitcoin" />
+        <AddAddress blockchain="Ethereum" />
+        <AddAddress blockchain="Solana" />
+      </div>
+      <CustomTable columns={columns} data={data} itemsPerPage={10} />
     </div>
   );
 }
