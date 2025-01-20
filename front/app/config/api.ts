910c792b447acd3d48b2721a967899a96dd58fd4
clement lmd <clement.laumond@gmail.com>
2025-01-20 17:42:39 +0100

    Remove .env.production files from git

--- front/app/config/api.ts
+++ front/app/config/api.ts
@@ -1,4 +1,4 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const API_URL = process.env.API_URL;
 
 if (!API_URL) {
   throw new Error('API_URL is not defined in environment variables');
