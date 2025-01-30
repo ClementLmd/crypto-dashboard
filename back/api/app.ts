c0b773d58d5975376e913dda424240c2d2a2846a
clement lmd <clement.laumond@gmail.com>
2025-01-30 10:38:50 +0100

    back deployment 16

--- back/api/app.ts
+++ back/api/app.ts
@@ -26,6 +26,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.get('/', (req, res) => res.send('Hello World'));
 app.use('/users', usersRoute);
 app.use('/addresses', addressesRoute);
 app.use('/auth', userSessionRoute);
